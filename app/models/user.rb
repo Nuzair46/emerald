c58ca7e769287ace778b877f47045602e30d0b7d
Nuzair46 <nuzer501@gmail.com>
2021-10-11 15:07:30 +0530

    Fixed order of items in ActiveRecord models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,34 +3,29 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
 
-  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
-  has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_many :comments, dependent: :destroy
+  has_many :tasks, dependent: :destroy, foreign_key: :user_id
+
   has_secure_password
   has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email, presence: true,
-                    uniqueness: true,
-                    length: { maximum: 50 },
-                    format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 50 },
+    format: { with: VALID_EMAIL_REGEX }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
 
-  has_many :comments, dependent: :destroy
-
   before_save :to_lowercase
-  before_destroy :assign_tasks_to_task_owners
 
   private
 
     def to_lowercase
       email.downcase!
-    end
-
-    def assign_tasks_to_task_owners
-      tasks_whose_owner_is_not_current_user = assigned_tasks.select { |task| task.task_owner_id != id }
-      tasks_whose_owner_is_not_current_user.each do |task|
-        task.update(assigned_user_id: task.task_owner_id)
-      end
     end
 end
