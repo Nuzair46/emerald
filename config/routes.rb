a52b054dc6016b9f92cee391fe528ee661957af2
Nuzair46 <nuzer501@gmail.com>
2021-10-13 16:11:11 +0530

    Added Heroku Sidekiq configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
+require "sidekiq/web"
+require "sidekiq/cron/web"
+
 Rails.application.routes.draw do
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+
+  draw :sidekiq
+
   defaults format: :json do
     resources :tasks, except: %i[new edit], param: :slug
-    resources :users, only: %i[index create]
-    resource :sessions, only: :create
+    resources :users, only: %i[create index]
     resource :sessions, only: %i[create destroy]
     resources :comments, only: :create
     resources :preferences, only: %i[show update]
